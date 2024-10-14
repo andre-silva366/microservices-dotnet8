e762fecd60cdedaeea9093364e7637d7137a5fad
andre-silva366 <andre.java.ads.2022@gmail.com>
2024-10-14 19:37:38 -0300

    Criado método PostAsJson e PutAsJson

--- GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace GeekShopping.Web.Utils;
 
 public static class HttpClientExtensions
 {
+    private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
     public static async Task<T> RedContentAs<T>(this HttpResponseMessage response)
     {
         if(!response.IsSuccessStatusCode)
@@ -13,5 +15,13 @@ public static class HttpClientExtensions
 
         var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         return JsonSerializer.Deserialize <T> (dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    }
+
+    public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+    {
+        var dataAsString = JsonSerializer.Serialize(data);
+        var content = new StringContent(dataAsString);
+        content.Headers.ContentType = contentType;
+        return httpClient.PostAsync(url, content);
     }
 }
